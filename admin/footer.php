da68c0e1588592b28ec1edbe0156705f2cc686a1
Wat2705 <vuvtvip102@gmail.com>
2023-11-06 20:12:24 +0700

    fix header

--- admin/footer.php
+++ admin/footer.php
@@ -1,5 +1,22 @@
-    </div>
+</div>
 
 </body>
+<script src="css/js/app.min.js"></script>
+<script src="css/js/vendor.min.js"></script>
+<script src="css/libs/morris-js/morris.min.js"></script>
+<script src="css/libs/raphael/raphael.min.js"></script>
+
+<script src="css/js/pages/dashboard.init.js"></script>
+<script>
+    switchDarkMode = () => {
+        if (document.getElementById("dark-mode-switch").checked) {
+            document.getElementById("bootstrap-stylesheet").href = "css/css/bootstrap-dark.min.css";
+            document.getElementById("app-stylesheet").href = "css/css/app-dark.min.css";
+        } else {
+            document.getElementById("bootstrap-stylesheet").href = "css/css/bootstrap.min.css";
+            document.getElementById("app-stylesheet").href = "css/css/app.min.css";
+        }
+    }
+</script>
 
 </html>
--- admin/header.php
+++ admin/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <title>Admin Dashboard</title>
@@ -14,11 +15,146 @@
     <link href="css/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
     <link href="css/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="css/css/app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet" />
-
+    <style>
+        .pos {
+            position: relative;
+            top: 25px;
+            left: 20px;
+        }
+    </style>
 </head>
 
 <body>
 
     <!-- Begin page -->
     <div id="wrapper">
+        <div class="navbar-custom">
+            <ul class="list-unstyled topnav-menu float-right mb-0">
+
+                <li class="dropdown d-none d-lg-block">
+                    <a class="nav-link dropdown-toggle mr-0" data-toggle="dropdown" href="#" role="button"
+                        aria-haspopup="false" aria-expanded="false">
+                        <img src="css/images/flags/us.jpg" alt="user-image" class="mr-2" height="12"> <span
+                            class="align-middle">English <i class="mdi mdi-chevron-down"></i> </span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <img src="css/images/flags/spain.jpg" alt="user-image" class="mr-2" height="12"> <span
+                                class="align-middle">Spanish</span>
+                        </a>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <img src="css/images/flags/italy.jpg" alt="user-image" class="mr-2" height="12"> <span
+                                class="align-middle">Italian</span>
+                        </a>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <img src="css/images/flags/french.jpg" alt="user-image" class="mr-2" height="12"> <span
+                                class="align-middle">French</span>
+                        </a>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <img src="css/images/flags/russia.jpg" alt="user-image" class="mr-2" height="12"> <span
+                                class="align-middle">Russian</span>
+                        </a>
+                    </div>
+                </li>
+
+                <li class="dropdown notification-list">
+                    <a class="nav-link dropdown-toggle nav-user mr-0" data-toggle="dropdown" href="#" role="button"
+                        aria-haspopup="false" aria-expanded="false">
+                        <span class="pro-user-name ml-1">
+                            Maxine K <i class="mdi mdi-chevron-down"></i>
+                        </span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+                        <!-- item-->
+                        <div class="dropdown-header noti-title">
+                            <h6 class="text-overflow m-0">Welcome !</h6>
+                        </div>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="mdi mdi-account-outline"></i>
+                            <span>Profile</span>
+                        </a>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="mdi mdi-lock-outline"></i>
+                            <span>Lock Screen</span>
+                        </a>
+
+                        <div class="dropdown-divider"></div>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="mdi mdi-logout-variant"></i>
+                            <span>Logout</span>
+                        </a>
+
+                    </div>
+                </li>
+
+            </ul>
+
+            <!-- LOGO -->
+            <!-- LOGO -->
+            <div class="logo-box">
+                <a href="index.html" class="logo text-center logo-dark">
+                    <span class="logo-lg">
+                        <img src="css/images/logo-dark.png" alt="" height="26">
+                        <!-- <span class="logo-lg-text-dark">Simple</span> -->
+                    </span>
+                    <span class="logo-sm">
+                        <!-- <span class="logo-lg-text-dark">S</span> -->
+                        <img src="css/images/logo-sm.png" alt="" height="22">
+                    </span>
+                </a>
+
+                <a href="index.html" class="logo text-center logo-light">
+                    <span class="logo-lg">
+                        <img src="css/images/logo-light.png" alt="" height="26">
+                        <!-- <span class="logo-lg-text-light">Simple</span> -->
+                    </span>
+                    <span class="logo-sm">
+                        <!-- <span class="logo-lg-text-light">S</span> -->
+                        <img src="css/images/logo-sm.png" alt="" height="22">
+                    </span>
+                </a>
+            </div>
+
+            <ul class="list-unstyled topnav-menu topnav-menu-left m-0">
+                <li>
+                    <button class="button-menu-mobile">
+                        <i class="mdi mdi-menu"></i>
+                    </button>
+                </li>
+                <li>
+                    <div class="custom-control custom-switch mb-3 pos">
+                        <input type="checkbox" class="custom-control-input theme-choice" id="dark-mode-switch"
+                            onclick="switchDarkMode()">
+                        <label class="custom-control-label" for="dark-mode-switch">Dark Mode</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
         <?php include "leftbar.php" ?>
+        <div class="content-page">
+            <div class="content">
+
+                <!-- Start container-fluid -->
+                <div class="container-fluid">
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div>
+                                <h4 class="header-title mb-3">Welcome ! </h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end row -->
