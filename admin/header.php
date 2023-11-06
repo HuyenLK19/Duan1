e7f2885daafd0daf817b6fb49dc98ed6b260191e
Wat2705 <vuvtvip102@gmail.com>
2023-11-06 14:57:51 +0700

    admin ui

---
+++ admin/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard | Simple - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Responsive bootstrap 4 admin template" name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="css/images/favicon.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <!-- App css -->
+    <link href="css/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+    <link href="css/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/css/app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet" />
+
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+        <?php include "leftbar.php" ?>
---
+++ admin/leftbar.php
@@ -0,0 +1,68 @@
+<div class="left-side-menu">
+
+
+    <div class="user-box">
+        <div class="user-info">
+            <p class="name">Stanley Jones</p>
+            <p class="text-muted m-0">Administrator</p>
+        </div>
+    </div>
+
+    <!--- Sidemenu -->
+    <div id="sidebar-menu">
+
+        <ul class="metismenu" id="side-menu">
+
+            <li class="menu-title">Navigation</li>
+
+            <li>
+                <a href="index.php">
+                    <i class="ti-home"></i>
+                    <span> Trang chủ </span>
+                </a>
+            </li>
+            <li>
+                <a href="index.html">
+                    <i class="ti-package"></i>
+                    <span> Sản phẩm </span>
+                </a>
+            </li>
+            <li>
+                <a href="index.html">
+                    <i class="ti-bookmark"></i>
+                    <span> Danh mục </span>
+                </a>
+            </li>
+            <li>
+                <a href="index.html">
+                    <i class="ti-comments"></i>
+                    <span> Bình luận </span>
+                </a>
+            </li>
+            <li>
+                <a href="index.html">
+                    <i class="ti-shopping-cart"></i>
+                    <span> Đơn hàng </span>
+                </a>
+            </li>
+            <li>
+                <a href="index.html">
+                    <i class="ti-receipt"></i>
+                    <span> Hóa đơn </span>
+                </a>
+            </li>
+            <li>
+                <a href="charts.php">
+                    <i class="ti-pie-chart"></i>
+                    <span> Thống kê </span>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+    <!-- End Sidebar -->
+
+    <div class="clearfix"></div>
+
+
+</div>
