6cd8cbe8428b1d1acca9eef5d4a106e2dff76902
HuyenLK19 <lkhuyen.mmiv@gmai.com>
2023-11-06 23:56:31 +0700

    controller

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,27 @@
 <?php
     include "header.php";
-    include "home.php";
+    if (isset($_GET['act']) && ($_GET['act'] != '')){
+        switch($_GET['act']){
+            case "addsp":
+                include "sanpham/add.php";
+                break;
+            case "adddm":
+                include "danhmuc/add.php";
+                break;
+            case "listbill":
+                include "donhang/list.php";
+                break;
+            case "listtk":
+                include "taikhoan/list.php";
+                break;
+            case "listbl":
+                include "binhluan/list.php";
+                break;
+            case "thongke":
+                include "thongke/chart.php";
+                break;
+                                
+        } 
+    }else include "home.php";
+
     include "footer.php";
-?>
