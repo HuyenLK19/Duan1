1b83dc6bebee5fc67006edd7a081e36a16d8b8c7
HuyenLK19 <lkhuyen.mmiv@gmai.com>
2023-11-08 13:55:44 +0700

    new

---
+++ admin/binhluan/list.php
@@ -0,0 +1,171 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <h5 class="mt-0 font-14 mb-3">Bình luận</h5>
+            <div class="table-responsive">
+                <table class="table table-hover mails m-0 table table-actions-bar table-centered">
+                    <thead>
+                        <tr>
+                            <th style="min-width: 20px;">ID</th>
+                            <th>Nội dung</th>
+                            <th>Người gửi</th>
+                            <th>Sản phẩm đánh giá</th>
+                            <th>Ngày đánh giá</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>
+                                1
+                            </td>
+                            <td>
+                                Sản phẩm đẹp
+                            </td>
+
+                            <td>
+                                <a>tomaslau</a>
+                            </td>
+
+                            <td>
+                                Áo polo unisex
+                            </td>
+
+                            <td>
+                                01/01/2023
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                2
+                            </td>
+
+                            <td>
+                                Sản phẩm đẹp
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">chadengle</a>
+                            </td>
+
+                            <td>
+                            Áo polo unisex
+                            </td>
+
+                            <td>
+                                01/01/2023
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                3
+                            <td>
+                                Sản phẩm đẹp
+
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">stillnotdavi</a>
+                            </td>
+                            <td>
+                            Áo polo unisex
+                            </td>
+
+                            <td>
+                                01/01/2023
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>
+
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                4
+                            </td>
+
+                            <td>
+                                Sản phẩm đẹp
+
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">kurafire</a>
+                            </td>
+
+                            <td>
+                            Áo polo unisex
+                            </td>
+
+                            <td>
+                                01/01/2023
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>
+
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                5
+                            </td>
+
+                            <td>
+                                Sản phẩm đẹp
+
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">shahedk</a>
+                            </td>
+
+                            <td>
+                            Áo polo unisex
+                            </td>
+
+                            <td>
+                                01/01/2023
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>
+
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                6
+                            </td>
+
+                            <td>
+                                Sản phẩm đẹp
+
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">adhamdannaway</a>
+                            </td>
+
+                            <td>
+                            Áo polo unisex
+                            </td>
+
+                            <td>
+                                01/01/2023
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ admin/taikhoan/list.php
@@ -0,0 +1,168 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <h5 class="mt-0 font-14 mb-3">Người dùng</h5>
+            <div class="table-responsive">
+                <table class="table table-hover mails m-0 table table-actions-bar table-centered">
+                    <thead>
+                        <tr>
+                            <th style="min-width: 20px;">
+                            </th>
+                            <th>Tên</th>
+                            <th>Email</th>
+                            <th>Vai trò</th>
+                            <th>Mật khẩu</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>
+                            </td>
+                            <td>
+                                Tomaslau
+                            </td>
+
+                            <td>
+                                <a>tomaslau@dummy.com</a>
+                            </td>
+
+                            <td>
+                                <b><a href="#" class="text-dark"><b>Admin</b></a>
+                                </b>
+                            </td>
+
+                            <td>
+                                123
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>                  
+                    
+                        </tr>
+
+                        <tr>
+                            <td>
+                            </td>
+
+                            <td>
+                                Chadengle
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">chadengle@dummy.com</a>
+                            </td>
+
+                            <td>
+                                <b><a href="#" class="text-dark"><b>User</b></a>
+                                </b>
+                            </td>
+
+                            <td>
+                                123
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>                  
+                        
+                        </tr>
+
+                        <tr>
+                            <td>
+                            <td>
+                                Stillnotdavid
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">stillnotdavid@dummy.com</a>
+                            </td>
+                            <td>
+                                <b><a href="#" class="text-dark"><b>User</b></a>
+                                </b>
+                            </td>
+
+                            <td>
+                                123
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>                  
+                      
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                            </td>
+
+                            <td>
+                                Kurafire
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">kurafire@dummy.com</a>
+                            </td>
+
+                            <td>
+                                <b><a href="#" class="text-dark"><b>User</b></a>
+                                </b>
+                            </td>
+
+                            <td>
+                                123
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>                  
+                     
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                            </td>
+
+                            <td>
+                                Shahedk
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">shahedk@dummy.com</a>
+                            </td>
+
+                            <td>
+                                <b><a href="#" class="text-dark"><b>User</b></a>
+                                </b>
+                            </td>
+
+                            <td>
+                                123
+                            </td>
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>                  
+                     
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                            </td>
+
+                            <td>
+                                Adhamdannaway
+                            </td>
+
+                            <td>
+                                <a href="#" class="text-muted">adhamdannaway@dummy.com</a>
+                            </td>
+
+                            <td>
+                                <b><a href="#" class="text-dark"><b>Admin</b></a>
+                                </b>
+                            </td>
+
+                            <td>
+                                123
+                            </td>  
+                            <td><a href=""><i class=" mdi mdi-trash-can-outline"></i></a></td>                  
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
