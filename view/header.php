a8958eb981925a9815e95d9de7c4e39beb40c0ac
HuyenLK19 <lkhuyen.mmiv@gmai.com>
2023-11-10 19:26:32 +0700

    cmmm

--- view/header.php
+++ view/header.php
@@ -134,12 +134,7 @@
                                                             <div class="product-image dropcart__product-image"><a
                                                                     href="product.html" class="product-image__body"><img
                                                                         class="product-image__img"
-<<<<<<< HEAD
                                                                         src="css/images/products/anh1.webp" alt=""></a>
-=======
-                                                                        src="css/images/products/product-1.jpg"
-                                                                        alt=""></a>
->>>>>>> 162ad0aba3ad98de7851f809955308db8e9489c2
                                                             </div>
                                                             <div class="dropcart__product-info">
                                                                 <div class="dropcart__product-name"><a
@@ -163,12 +158,7 @@
                                                             <div class="product-image dropcart__product-image"><a
                                                                     href="product.html" class="product-image__body"><img
                                                                         class="product-image__img"
-<<<<<<< HEAD
                                                                         src="css/images/products/anh7.webp" alt=""></a>
-=======
-                                                                        src="css/images/products/product-2.jpg"
-                                                                        alt=""></a>
->>>>>>> 162ad0aba3ad98de7851f809955308db8e9489c2
                                                             </div>
                                                             <div class="dropcart__product-info">
                                                                 <div class="dropcart__product-name"><a
@@ -188,12 +178,7 @@
                                                             <div class="product-image dropcart__product-image"><a
                                                                     href="product.html" class="product-image__body"><img
                                                                         class="product-image__img"
-<<<<<<< HEAD
                                                                         src="css/images/products/anh5.webp" alt=""></a>
-=======
-                                                                        src="css/images/products/product-5.jpg"
-                                                                        alt=""></a>
->>>>>>> 162ad0aba3ad98de7851f809955308db8e9489c2
                                                             </div>
                                                             <div class="dropcart__product-info">
                                                                 <div class="dropcart__product-name"><a
@@ -238,16 +223,10 @@ Tổng phụ</th>
                                                         </table>
                                                     </div>
                                                     <div class="dropcart__buttons"><a class="btn btn-secondary"
-<<<<<<< HEAD
                                                             href="index.php?act=cart">Xem giỏ hàng</a> <a class="btn btn-primary"
                                                             href="index.php?act=checkout">Đơn hàng</a></div>
-=======
-                                                            href="index.php?act=cart">Giỏ hàng</a> <a
-                                                            class="btn btn-primary" href="index.php?act=checkout">Thanh
-                                                            toán</a></div>
->>>>>>> 162ad0aba3ad98de7851f809955308db8e9489c2
                                                 </div>
-                                            </div><!-- .dropcart / end -->
+                                            </div>.dropcart / end
                                         </div>
                                     </div>
                                     <div class="indicator indicator--trigger--click">
