43977aa962f4eee2c0ad5001c5d71c4adcaa6048
Wat2705 <vuvtvip102@gmail.com>
2023-11-10 03:01:55 -0500

    update home

--- admin/home.php
+++ admin/home.php
@@ -13,7 +13,7 @@
                 <div class="row">
                     <div class="col-xl-3 col-sm-6 widget-inline-box">
                         <div class="text-center p-3">
-                            <h2 class="mt-2"><i class="text-primary mdi mdi-access-point-network mr-2"></i> <b>8954</b>
+                            <h2 class="mt-2"><i class="text-primary mdi mdi-tshirt-crew mr-2"></i> <b>8954</b>
                             </h2>
                             <p class="text-muted mb-0">Tổng số lượng sản phẩm</p>
                         </div>
@@ -21,21 +21,21 @@
 
                     <div class="col-xl-3 col-sm-6 widget-inline-box">
                         <div class="text-center p-3">
-                            <h2 class="mt-2"><i class="text-teal mdi mdi-airplay mr-2"></i> <b>7841</b></h2>
+                            <h2 class="mt-2"><i class="text-teal  mdi mdi-account-card-details mr-2"></i> <b>7841</b></h2>
                             <p class="text-muted mb-0">Tổng số người dùng</p>
                         </div>
                     </div>
 
                     <div class="col-xl-3 col-sm-6 widget-inline-box">
                         <div class="text-center p-3">
-                            <h2 class="mt-2"><i class="text-info mdi mdi-black-mesa mr-2"></i> <b>6521</b></h2>
+                            <h2 class="mt-2"><i class="text-info mdi mdi-check-box-multiple-outline mr-2"></i> <b>6521</b></h2>
                             <p class="text-muted mb-0">Tổng số đơn hàng</p>
                         </div>
                     </div>
 
                     <div class="col-xl-3 col-sm-6">
                         <div class="text-center p-3">
-                            <h2 class="mt-2"><i class="text-danger mdi mdi-cellphone-link mr-2"></i> <b>325</b></h2>
+                            <h2 class="mt-2"><i class="text-danger mdi mdi-comment mr-2"></i> <b>325</b></h2>
                             <p class="text-muted mb-0">Tổng số lượt bình luận</p>
                         </div>
                     </div>
