11102b23d2dbae9303f692f96b86a8db020cb9de
nguyenhien0123 <135849245+nguyenhien0123@users.noreply.github.com>
2023-11-29 16:49:54 +0700

    commit code

--- model/donhang.php
+++ model/donhang.php
@@ -1,11 +1,22 @@
 <?php
     function listall_donhang(){
-        $sql = "SELECT dh.id, dh.pttt, dh.ngaydathang, dh.pttt, dh.status, dh.status, tk.name as tentk, sp.price as giasp, sp.name as tensp FROM donhang dh INNER JOIN taikhoan tk ON dh.iduser = tk.id INNER JOIN sanpham sp ON dh.idpro = sp.id ORDER BY dh.id ASC";
+        $sql = "SELECT dh.id, dh.pttt, dh.ngaydathang, dh.status, tk.name as tentk, sp.price as giasp, sp.name as tensp FROM donhang dh INNER JOIN taikhoan tk ON dh.iduser = tk.id INNER JOIN sanpham sp ON dh.idpro = sp.id ORDER BY dh.id ASC";
         return pdo_query($sql);
+        
     }
-
     function listone_donhang($id){
-        $sql = "SELECT dh.id, dh.soluong, dh.pttt, dh.ngaydathang, dh.pttt, dh.status, dh.status, tk.name as tentk, sp.price as giasp, sp.name as tensp, tk.address as diachi, tk.tel FROM donhang dh INNER JOIN taikhoan tk ON dh.iduser = tk.id INNER JOIN sanpham sp ON dh.idpro = sp.id WHERE dh.id = '$id'";
+        $sql = "SELECT dh.id, ct.soluong, dh.pttt, dh.ngaydathang, dh.status, tk.name as tentk, sp.price as giasp, sp.name as tensp, tk.address as diachi, tk.tel FROM donhang dh INNER JOIN taikhoan tk ON dh.iduser = tk.id INNER JOIN sanpham sp ON dh.idpro = sp.id INNER JOIN cart ct ON dh.id = ct.idbill WHERE dh.id = '$id'";
         return pdo_query_one($sql);
+    }
+    
+    function loadall_bill(){
+        $sql = "select * from donhang order by id desc";
+        $listdonhang=pdo_query($sql);
+        return $listdonhang;
+    }
+    function lichsumua($iduser){
+        $sql="SELECT * FROM donhang WHERE iduser = $iduser ORDER BY id DESC";
+        $lichsumua=pdo_query($sql);
+        return $lichsumua;
     }
 ?>
